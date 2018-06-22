30836fa43476cd22989bd4eb0fe76ccb8fab66c4
Salman Thaikat <salmanthaikkat@gmail.com>
2018-06-22 17:18:21 +0530

    Mongoose ORM implemented

--- route.js
+++ route.js
@@ -4,7 +4,20 @@ var router=express.Router();
 var User=require('./user');
 
 router.get('/',(req,res)=>{
-    res.send("Hello world");
+    var user={
+        first_name:"Kick",
+        last_name:"Buttowski",
+        email:"kick@abc.com"
+    };
+    var newUser=User(user);
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("User added succesfully");
+        }
+    })
 });
 
 module.exports=router;
