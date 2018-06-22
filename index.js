193d93a3241ca84b86748a2fda47eac77bd5c9e0
Salman Thaikat <salmanthaikkat@gmail.com>
2018-06-22 16:58:21 +0530

    Added Routes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var port=process.env.PORT||8080;
 var app=express();
+var route=require('./route');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use('/',route);
 
 app.listen(port,(req,res)=>{
     console.log(`App is running at ${port}`);
